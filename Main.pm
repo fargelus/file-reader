d48b7356f11854fdf069f82fc467b22b1f1a5c27
fargelus <ddraudred@gmail.com>
2019-06-20 09:01:04 +0300

    View file content subroutine

--- Main.pm
+++ Main.pm
@@ -11,82 +11,65 @@ use Cwd;
 use File::Slurp;
 use File::Basename;
 
+my @G_WIDGETS_LIST = qw /Window Label Text Photo Img_btn Open_btn Save_btn/;
+my %G_WIDGETS = map {$_ => undef} @G_WIDGETS_LIST;
+
 sub main {
   my $mw = MainWindow->new;
+  $G_WIDGETS{'Window'} = $mw;
 
-  initWindowMeta($mw);
-  my (
-    $text_area,
-    $file_path_lbl,
-    $photo_obj,
-    $img_btn,
-    $open_btn,
-    $save_btn
-    ) = initUI($mw);
+  initWindowMeta();
+  initUI();
 
   my $filepath;
+  my $file_path_lbl = $G_WIDGETS{'Label'};
   $file_path_lbl->configure(-textvariable => \$filepath);
 
+  my $open_btn = $G_WIDGETS{'Open_btn'};
   $open_btn->configure(-command => sub {
-    $filepath = getUserSelectedFile($mw, $filepath);
-    my $content;
-    my $is_image = isImage($filepath);
+    $filepath = getUserSelectedFile($filepath);
 
-    if ($is_image) {
-      $text_area->packForget;
-
-      my $ext = getFileExtension($filepath);
-      $photo_obj->blank;
-      $photo_obj->configure(-file => $filepath, -format => $ext);
-
-      $img_btn->configure(-image => $photo_obj);
-      $img_btn->pack(-side => 'top');
-    } else {
-      $img_btn->packForget;
-      packTextArea($text_area);
-
-      $content = readFileContent($filepath);
-      $text_area->Contents($content);
+    if ($filepath) {
+      viewFileContent();
     }
-
-    my $state = defineSaveBtnState($content, $is_image);
-    $save_btn->configure(-state => $state);
   });
 
+  my $save_btn = $G_WIDGETS{'Save_btn'};
   $save_btn->configure(-command => sub {
-    my $updated = $text_area->Contents();
+    my $updated = $G_WIDGETS{'Text'}->Contents();
 
     my $msg = 'File was saved';
     $msg = 'Error, while saving file' unless (write_file($filepath, $updated));
     $mw->messageBox(-message => $msg);
   });
 
-  $file_path_lbl->pack(-side => 'top', -pady => 5);
-  packTextArea($text_area);
-
   MainLoop;
 }
 
 sub initWindowMeta {
-  my $win = shift;
+  my $win = $G_WIDGETS{'Window'};
 
   $win->title('File Viewer');
+
+  $win->bind('<Configure>' => sub {
+    my $w = shift;
+    $w->minsize($w->Width, $w->Height);
+    $w->bind('<Configure>' => sub {});
+  });
 }
 
 sub initUI {
-  my $mw = shift;
+  my $mw = $G_WIDGETS{'Window'};
 
-  my $text_area = $mw->Text;
-  my $file_path_lbl = $mw->Label;
-  my $photo_obj = $mw->Photo;
-  my $img_btn = $mw->Button;
-  my ($save_btn, $open_btn) = packBtns($mw);
-
-  ($text_area, $file_path_lbl, $photo_obj, $img_btn, $open_btn, $save_btn);
+  $G_WIDGETS{'Text'} = $mw->Text;
+  $G_WIDGETS{'Label'} = $mw->Label;
+  $G_WIDGETS{'Photo'} = $mw->Photo;
+  $G_WIDGETS{'Img_btn'} = $mw->Button;
+  packControlBtns();
 }
 
-sub packBtns {
-  my $mw = shift;
+sub packControlBtns {
+  my $mw = $G_WIDGETS{'Window'};
 
   my $btn_frame = $mw->Frame;
   my $save_btn = $btn_frame->Button(-text => 'Save', -state => 'disabled');
@@ -100,7 +83,50 @@ sub packBtns {
   $save_btn->pack(%btn_pos);
   $open_btn->pack(%btn_pos);
 
-  ($save_btn, $open_btn);
+  $G_WIDGETS{'Save_btn'} = $save_btn;
+  $G_WIDGETS{'Open_btn'} = $open_btn;
+}
+
+sub getUserSelectedFile {
+  my $path = shift;
+
+  my $dir = $path ? dirname($path) : getcwd;
+  my $mw = $G_WIDGETS{'Window'};
+  my $f_select = $mw->FileSelect(-directory => $dir);
+  $f_select->Show;
+}
+
+sub viewFileContent {
+  my $file_path_lbl = $G_WIDGETS{'Label'};
+  my $filepath = $file_path_lbl->cget('-text');
+
+  my $mw = $G_WIDGETS{'Window'};
+  my $text_area = $G_WIDGETS{'Text'};
+  my $img_btn = $G_WIDGETS{'Img_btn'};
+  my $is_image = isImage($filepath);
+  my $content;
+
+  $file_path_lbl->pack(-side => 'top', -pady => 5);
+  if ($is_image) {
+    $text_area->packForget;
+
+    my $ext = getFileExtension($filepath);
+    my $photo_obj = $G_WIDGETS{'Photo'};
+    $photo_obj->blank;
+    $photo_obj->configure(-file => $filepath, -format => $ext);
+
+    $img_btn->configure(-image => $photo_obj);
+    $img_btn->pack(-side => 'top');
+  } else {
+    $img_btn->packForget;
+    $text_area->pack(-side => 'top');
+
+    $content = readFileContent($filepath);
+    $text_area->Contents($content);
+  }
+
+  my $state = defineSaveBtnState($content, $is_image);
+  $G_WIDGETS{'Save_btn'}->configure(-state => $state);
 }
 
 sub isImage {
@@ -115,19 +141,6 @@ sub getFileExtension {
   my $path = shift;
 
   $path =~ /.([^.]+)$/ ? $1 : '';
-}
-
-sub packTextArea {
-  my $text = shift;
-  $text->pack(-side => 'top')
-}
-
-sub getUserSelectedFile {
-  my ($mw, $path) = @_;
-
-  my $dir = $path ? dirname($path) : getcwd;
-  my $f_select = $mw->FileSelect(-directory => $dir);
-  $f_select->Show;
 }
 
 sub readFileContent {
